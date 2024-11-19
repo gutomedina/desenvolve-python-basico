a47e2ad97a16ac567c6067f3db2cf0812d88e0f4
gutomedina <augusto.ferreira@pditabira.com>
2024-11-19 14:59:39 -0300

    Add files via upload

---
+++ Módulo2/aula2_questão2.py
@@ -0,0 +1,17 @@
+texto = "o resultado é"
+valor1 = 10
+valor2 = 3.5
+
+resultado = valor1 + valor2
+
+print(texto)
+print(type(texto))
+
+print(valor1)
+print(type(valor1))
+
+print(valor2)
+print(type(valor2))
+
+print(resultado)
+print(type(resultado))
