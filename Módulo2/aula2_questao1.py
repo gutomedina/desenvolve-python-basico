caf4bfca78e44cc6fe197c0880ae6c454689027b
gutomedina <augusto.ferreira@pditabira.com>
2024-11-19 14:57:38 -0300

    Add files via upload

---
+++ Módulo2/aula2_questao1.py
@@ -0,0 +1,14 @@
+# Armazenando os valores nas variáveis
+var1 = 5
+var2 = 2
+
+# Imprimindo o tipo de dados das duas variáveis
+print(type(var1))
+print(type(var2))
+
+# Dividindo var1 por var2 e armazenando o resultado em uma terceira variável
+var3 = var1 / var2
+
+# Imprimindo a terceira variável e o seu tipo
+print(var3)
+print(type(var3))
